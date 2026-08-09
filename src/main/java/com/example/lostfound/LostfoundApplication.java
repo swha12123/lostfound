f51b1af34bc3afd6becdea60bf01dd1fdf85a17f
swha12123 <swha12123@users.noreply.github.com>
2026-08-10 02:41:37 +0900

    스프링 부트 진입점 추가 (LostfoundApplication)
    @SpringBootApplication 메인 클래스.

---
+++ src/main/java/com/example/lostfound/LostfoundApplication.java
@@ -0,0 +1,11 @@
+package com.example.lostfound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostfoundApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LostfoundApplication.class, args);
+    }
+}
